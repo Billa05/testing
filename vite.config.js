a7653774dd4f30955185ccda04442a6355e7df63
billa05 <workinguse5@gmail.com>
2025-02-12 02:35:18 +0530

    testing workflow

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,11 @@ import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
- plugins: [react()],
- resolve: {
-   alias: {
-     "@": path.resolve(__dirname, "./src"),
-   },
- },
+  ase: "/testing/",
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
