e3f6b3e8ceb3a001c3a8484a374bd7d98c0b8945
billa05 <workinguse5@gmail.com>
2025-02-12 02:40:12 +0530

    testing workflow

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  ase: "/testing/",
+  base: "/testing/",
   plugins: [react()],
   resolve: {
     alias: {
